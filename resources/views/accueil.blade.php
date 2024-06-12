2f52668025369c4631afe3fada33bcff2b6c1e44
Haps <thiamhapstou@gmail.com>
2024-06-12 15:49:25 +0000

    création de la page d'accueil avec html

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,147 @@
+
+
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="{{asset('images/banner1.png')}}" class="d-block w-100" alt="Image 1">
+          </div>
+          <div class="col-md-6">
+            <h2>SAS HACKEUSES</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button class="btn btn-primary">Nos  FORMATIONS</button>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="row">
+          <div class="col-md-6 order-md-2">
+            <img src="{{asset('images/banner3.png')}}" class="d-block w-100" alt="Image 2">
+          </div>
+          <div class="col-md-6 order-md-1">
+            <h2>Des  FORMATIONS aux metiers du NUMÉRIQUE</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button class="btn btn-primary">Nos  FORMATIONS</button>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="{{asset('images/banner2.png')}}" class="d-block w-100" alt="Image 3">
+          </div>
+          <div class="col-md-6">
+            <h2>GRADUATION</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button class="btn btn-primary">Nos  FORMATIONS</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="{{asset('images/propos.jpg')}}" alt="Image de gauche">
+        </div>
+        <div class="col-md-6">
+          <h2>Titre de la section</h2>
+          <p>Premier paragraphe de la section</p>
+          <p>Deuxième paragraphe de la section</p>
+          <p>Troisième paragraphe de la section</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section">
+    <div class="container">
+        <div class="row">
+            <h2>Titre de la section</h2>
+            <p>Description de la section</p>
+          </div>
+      <div class="row">
+            <div class="col-md-6">
+                <div class="image-container">
+                  <img src="{{asset('images/image 1.jpg')}}" alt="Image 1">
+                  <img src="{{asset('images/image 7.png')}}" alt="Image 2">
+                  <img src="{{asset('images/image 5.jpg')}}" alt="Image 3">
+                  <img src="{{asset('images/image 6.jpg')}}" alt="Image 4">
+                </div>
+                <button class="btn btn-primary">Voir plus</button>
+              </div>
+              <div class="col-md-6">
+                <img src="{{asset('images/formation.jpg')}}" alt="">
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+        <div class="banner">
+          <img src="{{asset('images/banniere.JPG')}}" alt="Bannière">
+        </div>
+        <div class="container">
+          <h2>Titre de la section</h2>
+          <div class="counters">
+            <div class="counter">
+              <span class="count">20000</span>
+              <span>Personnes formées</span>
+            </div>
+            <div class="counter">
+              <span class="count">96</span>
+              <span>Sorties positives</span>
+            </div>
+            <div class="counter">
+              <span class="count">250</span>
+              <span>Partenaires</span>
+            </div>
+            <div class="counter">
+              <span class="count">13</span>
+              <span>Régions d'interventions</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section">
+        <div class="container">
+          <h2>Témoignages</h2>
+          <div class="content">
+            <div class="image-with-title">
+              <img src="image.jpg" alt="Image">
+              <h3>Titre de l'image</h3>
+            </div>
+            <div class="quote-card">
+              <blockquote>
+                <p>Contenu de la citation...</p>
+                <footer>Auteur de la citation</footer>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section">
+        <div class="container">
+          <h2>Partenaires</h2>
+          <div class="gallery">
+            <img src="{{asset('images/img1.png')}}" alt="Image 1">
+            <img src="{{asset('images/img2.png')}}" alt="Image 2">
+            <img src="{{asset('images/img3.png')}}" alt="Image 3">
+            <img src="{{asset('images/img4.png')}}" alt="Image 4">
+            <img src="{{asset('images/img5.png')}}" alt="Image 5">
+            <img src="{{asset('images/img6.png')}}" alt="Image 6">
+            <img src="{{asset('images/img7.png')}}" alt="Image 1">
+            <img src="{{asset('images/img8.png')}}" alt="Image 2">
+            <img src="{{asset('images/img9.png')}}" alt="Image 3">
+            <img src="{{asset('images/img10.png')}}">
+            <img src="{{asset('images/img11.png')}}">
+            <img src="{{asset('images/img12.png')}}">
+          </div>
+        </div>
+      </div>
+      
+      
+      
+  
+  
+  
