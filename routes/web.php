2a5c3d67fe9490f2a9df982aef2a7d9722c5f8e0
meblo98 <lomouhamedelbachir@gmail.com>
2024-06-12 16:07:04 +0000

    init page formation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\FormationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('formation', [FormationController::class, 'index']);
