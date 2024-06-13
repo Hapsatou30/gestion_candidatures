f49cbae67094b3a19f09117319a2fab824a7f7a5
Haps <thiamhapstou@gmail.com>
2024-06-13 10:08:29 +0000

    ajout d'image

---
+++ resources/views/formations/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+
+    <main class="container">
+        <div class="container text-center">
+            <div class="row">
+              <div class="col">
+                <h1>Trouver votre formation en 2
+                    clics
+                </h1>
+                <div class=''>
+                    <button class='btn btn-danger'>96% de nos apprennant recommandent Simplon Formation</button>
+                </div>
+              </div>
+              <div class="col">
+                <img src="{{ asset('images/banniere.png') }}" alt="">
+              </div>
+            </div>
+          </div>
+    </main>
+
+@endsection
